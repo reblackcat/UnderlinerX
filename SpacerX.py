bb0f2f6d593c920388342a916a27397efe19142e
prohansm <69315312+prohansm@users.noreply.github.com>
2023-11-22 14:20:22 +0800

    optimize the UnderlinerX.py and add the SpacerX.py

--- SpacerX.py
+++ SpacerX.py
@@ -3,12 +3,12 @@ import sys
 import re
 import shutil
 
-def replace_special_characters_with_underline(s):
-    return re.sub(r'[^\w.]', '_', s)
+def replace_special_characters_with_space(s):
+    return re.sub(r'[^\w\.]', ' ', s).replace('_', ' ')
 
-def remove_multiple_underline(s):
-    # 将连续的下划线替换为单个下划线
-    return re.sub(r'_+', '_', s)
+def remove_multiple_space(s):
+    # 将连续的空格替换为单个空格
+    return re.sub(r'\s+', ' ', s)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -23,8 +23,8 @@ if __name__ == '__main__':
                 os.chdir(dir_path)
             
             filename = filename.strip() # 去除字符串两端的空格
-            filename = replace_special_characters_with_underline(filename)
-            filename = remove_multiple_underline(filename)
+            filename = replace_special_characters_with_space(filename)
+            filename = remove_multiple_space(filename)
 
             dest_filepath = os.path.join(dir_path, filename)
             print("dest filepath:", dest_filepath)
@@ -35,8 +35,8 @@ if __name__ == '__main__':
                 shutil.copy(filepath, dest_filepath)
     else:
         filename = input("Please enter your filename: ")
-        filename = filename.strip().replace(" ", "_")
-        filename = remove_multiple_underline(filename)
+        filename = filename.strip().replace("_", " ")
+        filename = remove_multiple_space(filename)
         print(filename)
         input("请按「回车键」退出...")
 
